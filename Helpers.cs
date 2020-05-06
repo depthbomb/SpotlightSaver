9087e0156ab6f6abe5b8dc1d2dd1a9497e7a8a60
depthbomb <reesepup77@hotmail.com>
2020-05-05 21:35:07 -0500

    Initial

---
+++ Helpers.cs
@@ -0,0 +1,27 @@
+﻿namespace SpotlightSaver
+{
+    public static class Helpers
+    {
+        /// <summary>
+        /// Creates a <paramref name="directory"/> if it does not been created
+        /// </summary>
+        /// <param name="directory">Directory to check</param>
+        public static void CreateIfNotExists(this string directory)
+            => directory.CreateIfNotExists();
+
+        /// <summary>
+        /// Creates a <paramref name="directory"/> if it has not been <paramref name="created"/>
+        /// </summary>
+        /// <param name="directory">Directory to check</param>
+        /// <param name="created"></param>
+        public static void CreateIfNotExists(this string directory, out bool created)
+        {
+            created = System.IO.Directory.Exists(directory);
+
+            if (!created)
+            {
+                System.IO.Directory.CreateDirectory(directory);
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+# Spotlight Saver
+
+This app saves the beautiful Windows 10 spotlight wallpapers to your pictures folders. These images are switched out every few days so you can run this program every so often to get the new content.
+
+## Caveats
+* If you've somehow disabled `Microsoft.Windows.ContentDeliveryManager` then you will probably not get any new images and thus this app will not work very well
+* This only works on Windows 10 (post-Anniversary update)
+
+## Requirements
+* You will need .NET Framework 4.5+ installed, but you probably already have this.
